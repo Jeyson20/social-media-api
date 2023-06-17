d891fc01e392751033a171b770c3a01ad3f7e329
Jeyson Almonte <jjalmonte@humano.com.do>
2023-06-17 00:07:17 -0400

    adding configuration and apicontroller base

---
+++ src/SocialMedia.API/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ApiControllerBase : ControllerBase
+	{
+		private ISender? _mediador;
+		protected ISender Mediator
+		{
+			get
+			{
+#pragma warning disable CS8603 // Possible null reference return.
+				return _mediador ??= HttpContext.RequestServices.GetService<ISender>();
+#pragma warning restore CS8603 // Possible null reference return.
+			}
+		}
+	}
+}
--- src/SocialMedia.API/Program.cs
+++ src/SocialMedia.API/Program.cs
@@ -1,11 +1,23 @@
+using SocialMedia.Application;
 using SocialMedia.Infrastructure;
 using SocialMedia.Infrastructure.Persistence.Context;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Layers
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
-builder.Services.AddControllers();
+
+builder.Services.AddRouting(x => x.LowercaseUrls = true);
+builder.Services.AddControllers().AddJsonOptions(json =>
+{
+	json.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+	json.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
