3c8a27710c7d717117223b5624665dd907808258
Jeyson Almonte <jjalmonte@humano.com.do>
2023-06-18 19:10:11 -0400

    configuring jwt

--- src/SocialMedia.API/Controllers/ApiControllerBase.cs
+++ src/SocialMedia.API/Controllers/ApiControllerBase.cs
@@ -5,17 +5,9 @@ namespace SocialMedia.API.Controllers
 {
 	[ApiController]
 	[Route("api/[controller]")]
-	public class ApiControllerBase : ControllerBase
+	public abstract class ApiControllerBase : ControllerBase
 	{
-		private ISender? _mediador;
-		protected ISender Mediator
-		{
-			get
-			{
-#pragma warning disable CS8603 // Possible null reference return.
-				return _mediador ??= HttpContext.RequestServices.GetService<ISender>();
-#pragma warning restore CS8603 // Possible null reference return.
-			}
-		}
+		private ISender? _mediator;
+		protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
 	}
 }
--- src/SocialMedia.Infrastructure/DependencyInjection.cs
+++ src/SocialMedia.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using SocialMedia.Application.Common.Interfaces;
+using SocialMedia.Application.Common.Utils;
+using SocialMedia.Application.Common.Wrappers;
 using SocialMedia.Infrastructure.Persistence.Context;
 using SocialMedia.Infrastructure.Persistence.Interceptors;
 using SocialMedia.Infrastructure.Services;
+using System.Text;
 
 namespace SocialMedia.Infrastructure
 {
@@ -22,6 +30,58 @@ namespace SocialMedia.Infrastructure
 			services.AddScoped<ApplicationDbContextInitialiser>();
 
 			services.AddTransient<IDateTime, DateTimeService>();
+			services.AddScoped<IAuthService, AuthService>();
+			services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+			services.AddHttpContextAccessor();
+
+
+			var settings = new JsonSerializerSettings
+			{
+				ContractResolver = new CamelCasePropertyNamesContractResolver(),
+				NullValueHandling = NullValueHandling.Ignore
+			};
+
+
+			var jwtSettings = configuration.GetSection("JwtConfig");
+			services.Configure<JwtConfig>(jwtSettings);
+			var appSettings = jwtSettings.Get<JwtConfig>();
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateIssuerSigningKey = true,
+					ValidIssuer = appSettings?.Issuer,
+					ValidAudience = appSettings?.Audience,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettings?.Secret ?? ""))
+				};
+				options.Events = new JwtBearerEvents
+				{
+					OnChallenge = context =>
+					{
+						context.HandleResponse();
+						context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+						context.Response.ContentType = "application/json";
+						context.ErrorDescription = "Provide a valid JWT access token";
+
+						if (context.AuthenticateFailure != null && context.AuthenticateFailure.GetType() == typeof(SecurityTokenExpiredException))
+						{
+							var authenticationException = context.AuthenticateFailure as SecurityTokenExpiredException;
+							context.Response.Headers.Add("x-token-expired", "true");
+							context.ErrorDescription = $"The token expired on {authenticationException?.Expires.ToString("yyyy-MM-ddThh:mm:ss")}";
+						}
+						var result = new ApiResponse<string>(context.ErrorDescription);
+						return context.Response.WriteAsync(JsonConvert.SerializeObject(result, settings));
+					},
+				};
+			});
 
 			return services;
 		}
