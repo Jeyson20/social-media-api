340eda634903b45b04fc0dd4be273bee4e7677a9
Jeyson Almonte <jjalmonte@humano.com.do>
2023-07-17 00:34:01 -0400

    feat: add common PaginatedRequest

---
+++ src/SocialMedia.Application/Common/Wrappers/PaginatedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMedia.Application.Common.Wrappers
+{
+	public record PaginatedRequest 
+	{
+		public int PageNumber { get; init; } = 1;
+		public int PageSize { get; init; } = 50;
+	}
+}
