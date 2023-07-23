3fe4eb91daa2f3abbb2f915f01323efa21d7fff4
Jeyson Almonte <jeysom@live.com>
2023-07-23 02:41:40 -0400

    test: add application- refreshTokenCommandtests

---
+++ tests/SocialMedia.Application.UnitTests/Auth/Commands/RefreshTokenCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using SocialMedia.Application.Auth.Commands.RefreshToken;
+using SocialMedia.Application.Auth.DTOs;
+
+namespace SocialMedia.Application.UnitTests.Auth.Commands;
+
+public class RefreshTokenCommandTests
+{
+	[Fact]
+	public async Task RefreshTokenCommandHandler_ShouldReturnAuthDto_WhenRefreshTokenIsValid()
+	{
+		// Arrange
+		var authServiceMock = new Mock<IAuthService>();
+		var cancellationToken = new CancellationToken();
+
+		authServiceMock.Setup(x => x.RefreshTokenAsync(It.IsAny<RefreshTokenCommand>(), cancellationToken))
+			.ReturnsAsync(new TokenDto("your-access-token"));
+
+		var commandHandler = new RefreshTokenCommandHandler(authServiceMock.Object);
+		var command = new RefreshTokenCommand("testRefreshToken");
+
+		// Act
+		var result = await commandHandler.Handle(command, cancellationToken);
+
+		// Assert
+		result.Should().NotBeNull();
+		result.Token.Should().Be("your-access-token");
+	}
+
+	[Fact]
+	public async Task RefreshTokenCommandHandler_ShouldThrowApiException_WhenRefreshTokenIsInvalid()
+	{
+		// Arrange
+		var authServiceMock = new Mock<IAuthService>();
+		var cancellationToken = new CancellationToken();
+
+		authServiceMock.Setup(x => x.RefreshTokenAsync(It.IsAny<RefreshTokenCommand>(), cancellationToken))
+			.ThrowsAsync(new ApiException());
+
+		var commandHandler = new RefreshTokenCommandHandler(authServiceMock.Object);
+		var command = new RefreshTokenCommand("testRefreshToken");
+
+		// Act
+		Func<Task> act = async () => await commandHandler.Handle(command, cancellationToken);
+
+		// Assert
+		await act.Should().ThrowAsync<ApiException>();
+	}
+}
---
+++ tests/SocialMedia.Application.UnitTests/Usings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using FluentAssertions;
+global using Moq;
+global using AutoMapper;
+global using SocialMedia.Domain.Entities;
+global using SocialMedia.Domain.Enums;
+global using SocialMedia.Application.Common.Interfaces;
+global using SocialMedia.Application.Common.Exceptions;
