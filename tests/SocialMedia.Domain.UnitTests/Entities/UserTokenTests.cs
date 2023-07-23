24756d47aaeed6db99ccccdfa8c225d7b9511a50
Jeyson Almonte <jeysom@live.com>
2023-07-23 02:34:43 -0400

    test: usertokenTests

---
+++ tests/SocialMedia.Domain.UnitTests/Entities/UserTokenTests.cs
@@ -0,0 +1,34 @@
+﻿using SocialMedia.Domain.Entities;
+
+namespace SocialMedia.Domain.UnitTests.Entities;
+public class UserTokenTests
+{
+    [Fact]
+    public void CreateUserToken_ShouldCreateAUserToken()
+    {
+        //Arrange
+        int userId = 1;
+        string token = "sampleToken";
+
+        //Act
+        var userToken = UserToken.Create(userId, token);
+
+        //Assert
+        userToken.Should().NotBeNull();
+        userToken.UserId.Should().Be(userId);
+        userToken.Token.Should().Be(token);
+    }
+    [Fact]
+    public void UpdateUserToken_ShouldUpdateAUserToken()
+    {
+        //Arrange
+        string newToken = "sampleToken";
+        var userToken = UserToken.Create(1, "sample");
+
+        //Act
+        userToken.Update(newToken);
+
+        //Assert
+        userToken.Token.Should().Be(newToken);
+    }
+}
