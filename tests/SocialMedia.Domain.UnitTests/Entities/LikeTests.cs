5f185b4a780dbcd4c6ee8564100da2b6fcb5cfa1
Jeyson Almonte <jeysom@live.com>
2023-07-23 02:35:05 -0400

    test: add likeTests

---
+++ tests/SocialMedia.Domain.UnitTests/Entities/LikeTests.cs
@@ -0,0 +1,23 @@
+﻿using SocialMedia.Domain.Entities;
+
+namespace SocialMedia.Domain.UnitTests.Entities;
+
+public class LikeTests
+{
+    [Fact]
+    public void Create_ShouldAddALike()
+    {
+        //Arrange
+        int userId = 1;
+        int postId = 1;
+
+        //Act
+        var newLike = Like.Create(userId, postId);
+
+        //Assert
+        newLike.Should().NotBeNull();
+        newLike.UserId.Should().Be(userId);
+        newLike.PostId.Should().Be(postId);
+
+    }
+}
