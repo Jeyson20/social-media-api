ee46b849723d5b15cabd6bd62a83792b54e588d4
Jeyson Almonte <jeysom@live.com>
2023-07-23 02:33:56 -0400

    test: add commentTests

---
+++ tests/SocialMedia.Domain.UnitTests/Entities/CommentTests.cs
@@ -0,0 +1,25 @@
+﻿using SocialMedia.Domain.Entities;
+
+namespace SocialMedia.Domain.UnitTests.Entities;
+
+public class CommentTests
+{
+    [Fact]
+    public void Create_ShouldCreateANewComment()
+    {
+        //Arrange
+        int userId = 1;
+        int postId = 1;
+        string text = "test";
+
+        //Act
+        var newComment = Comment.Create(userId, postId, text);
+
+        //Assert
+        newComment.Should().NotBeNull();
+        newComment.UserId.Should().Be(userId);
+        newComment.PostId.Should().Be(postId);
+        newComment.Text.Should().Be(text);
+    }
+
+}
